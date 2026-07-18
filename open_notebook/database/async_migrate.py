333da00ba62e0ab938d8fbffb7c6d5f6e3948c7c
David Patton <dpatton01@student.fullsail.edu>
2026-07-18 00:57:29 -0400

    Migration 24: fix tenant PERMISSIONS to use \.role + \ (RecordID) avoiding \.field circular read

--- open_notebook/database/async_migrate.py
+++ open_notebook/database/async_migrate.py
@@ -146,6 +146,9 @@ class AsyncMigrationManager:
             AsyncMigration.from_file(
                 "open_notebook/database/migrations/23.surrealql"
             ),
+            AsyncMigration.from_file(
+                "open_notebook/database/migrations/24.surrealql"
+            ),
         ]
         self.down_migrations = [
             AsyncMigration.from_file(
@@ -216,6 +219,9 @@ class AsyncMigrationManager:
             ),
             AsyncMigration.from_file(
                 "open_notebook/database/migrations/23_down.surrealql"
+            ),
+            AsyncMigration.from_file(
+                "open_notebook/database/migrations/24_down.surrealql"
             ),
         ]
         self.runner = AsyncMigrationRunner(
